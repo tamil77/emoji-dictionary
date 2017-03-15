2b2fcab2c9500b54ca2d62a8c62c64b949367ed1
Tamilarasan Lakshmanan <tamilarasan.lakshmanan@multco.us>
2017-03-15 00:06:47 -0700

    TableView Added

--- EmojiRef/ViewController.swift
+++ EmojiRef/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var theCoolTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        theCoolTableView.dataSource = self
+        theCoolTableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "😎HELLO World!!"
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
