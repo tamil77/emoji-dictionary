4157a75678d6a7421317fba88e16baf34e174f3a
Tamilarasan Lakshmanan <tamilarasan.lakshmanan@multco.us>
2017-03-15 00:32:27 -0700

    Navigation Controller

--- EmojiRef/ViewController.swift
+++ EmojiRef/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "⌚️")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
